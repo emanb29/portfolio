aafe2ac3639e35f7f2f8cafbf14ae7c09273d628
emanb29 <ebgamer29@gmail.com>
2015-06-20 21:18:00 -0700

    Adding some main page content and refactoring other pages to fit my purposes. Added font-awesome too

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,15 +19,32 @@ angular
     'ngSanitize',
     'ngTouch'
   ])
+  .run(function($rootScope){
+    $rootScope.$on('$locationChangeSuccess', function(event, newURL, oldURL) {
+      //console.log(newURL, oldURL);
+      $('.nav.navbar-nav li').removeClass('active');
+      $('.nav.navbar-nav li a').each(function(i, $elem) {
+        if ($elem.href == newURL) {
+          var $this = $(this);
+          $this.parent().addClass('active');
+          document.title = 'Ethan Bell - '+$this.text();
+        }
+      });
+    });
+  })
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
+      .when('/demos', {
+        templateUrl: '../views/demos.html',
         controller: 'AboutCtrl'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'ContactCtrl'
       })
       .otherwise({
         redirectTo: '/'
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,81 @@
  */
 angular.module('portfolioApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.skills = [
+      {
+        name: 'JavaScript',
+        percent: 95,
+        color: 'light-blue'
+      },
+      {
+        name: 'jQuery',
+        percent: 85,
+        color: 'cyan'
+      },
+      {
+        name: 'node.js',
+        percent: 70,
+        color: 'teal'
+      },
+      {
+        name: 'AngularJS',
+        percent: 80,
+        color: 'green'
+      },
+      {
+        name: 'ExpressJS',
+        percent: 75,
+        color: 'light-green'
+      },
+      {
+        name: 'ECMAScript 6',
+        percent: 90,
+        color: 'lime'
+      },
+      {
+        name: 'Chrome',
+        percent: 95,
+        color: 'yellow'
+      },
+      {
+        name: 'MongoDB',
+        percent: 80,
+        color: 'amber'
+      },
+      {
+        name: 'SQL/MySQL',
+        percent: 86,
+        color: 'orange'
+      },
+      {
+        name: 'PHP',
+        percent: 85,
+        color: 'deep-orange'
+      },
+      {
+        name: 'Laravel',
+        percent: 80,
+        color: 'red'
+      },
+      {
+        name: 'MVC/MVVM',
+        percent: 90,
+        color: 'pink'
+      },
+      {
+        name: 'OOP',
+        percent: 95,
+        color: 'purple'
+      },
+      {
+        name: 'Git',
+        percent: 85,
+        color: 'deep-purple'
+      },
+      {
+        name: 'Cordova/XDK',
+        percent: 90,
+        color: 'indigo'
+      }
     ];
   });
