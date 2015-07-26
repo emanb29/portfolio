afd9ebebd4c2bcac3b01e2865e878c00d2d34bc5
emanb29 <ebgamer29@gmail.com>
2015-07-25 23:05:40 -0700

    Added scaffold for contact and demos pages, made content wrapper fluid with a custom max width. Probably could do with variables, might look into later.

--- app/scripts/controllers/contact.js
+++ app/scripts/controllers/contact.js
@@ -9,9 +9,14 @@
  */
 angular.module('portfolioApp')
   .controller('ContactCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.socialLinks = {
+      facebook: {
+        icon: 'fa-facebook-square',
+        url: 'https://www.facebook.com/emanb29'
+      },
+      twitter: {
+        icon: 'fa-twitter',
+        url: 'http://twitter.com/emanb29'
+      }
+    }
   });
